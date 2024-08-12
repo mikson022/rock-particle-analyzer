b9fe701eae978dc5ee883f2a95903ee9749b5768
mikson022 <mikson022@gmail.com>
2024-08-12 20:50:45 +0200

    Refactor save_image function into modular components
    Restructure code placement
    Add whitespaces and comments

--- main.py
+++ main.py
@@ -6,6 +6,23 @@ import cv2
 
 
 
+# File operation
+def remove_extension(name):
+    name = os.path.splitext(name)[0]
+    return name
+
+def add_timestamp_and_png(name):
+    timestamp = int(time.time())
+    file_name = f'{name}_{timestamp}.png'
+    return file_name
+
+def save_image(file_name, image, path, message):
+    full_path = os.path.join(path, file_name)
+    os.makedirs(path, exist_ok=True)
+    cv2.imwrite(full_path, image)
+    print(f'{message} {full_path}')
+
+# Image processing
 def cv_show(name, image):
     img_height, img_width = image.shape[:2]
     
@@ -15,15 +32,6 @@ def cv_show(name, image):
         resized_image = image
         
     cv2.imshow(name, resized_image)
-
-def save_image(name, image, path, message):
-    timestamp = int(time.time())
-    name = os.path.splitext(name)[0]
-    file_name = f'{name}_{timestamp}.png'
-    full_path = os.path.join(path, file_name)
-    os.makedirs(path, exist_ok=True)
-    cv2.imwrite(full_path, image)
-    print(f'{message} {full_path}')
 
 def detect_edges(image, binary_threshold_low, binary_threshold_high):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -37,6 +45,7 @@ def detect_edges(image, binary_threshold_low, binary_threshold_high):
 
 
 
+# Load config.json
 config_path = os.path.join(os.path.dirname(__file__), 'config.json')
 with open(config_path, 'r') as config_file:
     config = json.load(config_file)
@@ -47,6 +56,11 @@ accepted_extensions = config['accepted_extensions_for_images']
 unprocessed_images_directory = config['unprocessed_images_directory']
 detected_particles_directory = config['detected_particles_directory']
 
+
+
+
+
+# Detect images and process them one by one
 unprocessed_image_files = [
     file for file in os.listdir(unprocessed_images_directory)
     if os.path.isfile(os.path.join(unprocessed_images_directory, file))
@@ -63,7 +77,10 @@ for image_filename in unprocessed_image_files:
     print("Displaying image:", path)
     image = cv2.imread(path)
     image = detect_edges(image, 200, 255)
-    save_image('Detect edges', image, detected_particles_directory, 'Edge detected image saved')
+    
+    new_image_name = add_timestamp_and_png(remove_extension(image_filename))
+    save_image(new_image_name, image, detected_particles_directory, 'Edge detected image saved')
+    
     cv_show(image_filename, image)
     cv2.waitKey()
     cv2.destroyAllWindows()
